e0188b138563a5762d660c27e44ef78d3397725f
Vio <Vio@Vio-PC.fios-router.home>
2020-05-21 15:38:31 -0400

    Please review and add some stuff. tnx

--- src/Sdet/QA.java
+++ src/Sdet/QA.java
@@ -1,9 +1,22 @@
 package Sdet;
 
+import java.util.HashMap;
+
 public class QA {
+	
 public static void main(String[] args) {
 	
 	System.out.println("Dorina here");
 	
+	// We needs methods here  and loop trough or iterate trough.... let's do it!
+	
+	HashMap<String, Boolean> map = new HashMap<>();
+
+	map.put("Roses", true);
+	map.put("Tulips", true);
+	map.put("Peonies", true);
+	map.put("Iris", false);
+	map.put(null, null);
+	map.put(null, false);
 }
 }
